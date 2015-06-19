b5cea2283628fe8c392fe53db8061943bf92902d
Karoly Torok <karoly.torok@z-cosulting.eu>
2015-06-19 08:30:41 +0200

    Startup fixed with the right event to listen

--- src/main/java/hu/sample/blogster/service/listener/StartupListener.java
+++ src/main/java/hu/sample/blogster/service/listener/StartupListener.java
@@ -3,25 +3,30 @@ package hu.sample.blogster.service.listener;
 import hu.sample.blogster.service.blog.PostService;
 import hu.sample.blogster.service.user.UserService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
-public class StartupListener implements ApplicationListener<ApplicationEvent>{
-	
+public class StartupListener implements
+		ApplicationListener<ContextRefreshedEvent> {
+
+	private final Logger LOGGER = LoggerFactory
+			.getLogger(StartupListener.class);
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private PostService postService;
 
 	@Override
-	public void onApplicationEvent(ApplicationEvent event) {
+	public void onApplicationEvent(final ContextRefreshedEvent event) {
+		LOGGER.info("Application started");
 		userService.saveMasterUser();
 		postService.saveDemoPost();
-	}	
-
-	
+	}
 }
